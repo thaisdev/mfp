b714f3f66a6af86fca77bcb9aafe7b4176544b3e
Thaís Oliveira <jts_oliveira@outlook.com>
2021-11-12 15:12:38 -0300

    add dashboard project

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,41 +1,54 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from "history";
 
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const AuthApp = lazy(() => import("./components/AuthApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header
             isSignedIn={isSignedIn}
             onSignOut={() => setIsSignedIn(false)}
           />
           <Suspense fallback={<Progress />}>
-            <Route path="/auth">
-              <AuthApp onSignIn={() => setIsSignedIn(true)} />
-            </Route>
-            <Route path="/" component={MarketingApp} />
+            <Switch>
+              <Route path="/auth">
+                <AuthApp onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardApp />
+              </Route>
+              <Route path="/" component={MarketingApp} />
+            </Switch>
           </Suspense>
-          <Switch>
-            <Route />
-          </Switch>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
